48b30593ef942594c37407d7bba44d6968340da6
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-02-25 18:55:40 +0100

    Task 2 script

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,7 +1,7 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const name = "Andrea"
+const name = "Andrea";
 console.log(name);
 
 
@@ -9,6 +9,14 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 7;
+const text = "Ronaldo";
+const bool = true;
+console.log(number);
+console.log(text);
+console.log(bool);
+
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
