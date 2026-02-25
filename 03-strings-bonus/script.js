e845a7529714546ce14cd5b9a909757ff857eaf8
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-02-25 19:17:36 +0100

    Task 5 bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -27,5 +27,5 @@ console.log(`${city} ${cityName}`); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = '';
+const upperCaseHello = 'HELLO';
 console.log(upperCaseHello); // Output: "HELLO"
